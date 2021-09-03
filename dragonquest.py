cf17c49db08913957e62e43dcdd9bc5546908f37
marco-peperoncino <marco.peperoncino@gmail.com>
2021-09-03 12:58:10 +0900

    もどる・すすむ等 コメント整理

--- dragonquest.py
+++ dragonquest.py
@@ -1,5 +1,6 @@
 import pyxel
 
+# 文字コード
 str_code = (
     0, 1, 2, 3, 4, 30, 31, 32, 33, 34,
     5, 6, 7, 8, 9, 44, 45, 46, 47, 48,
@@ -10,6 +11,7 @@ str_code = (
     35, 36, 37, 43
 )
 
+# 入力した文字が表示される所の座標
 pwd_pos = (
     (64, 48), (72, 48), (80, 48), (88, 48), (96, 48),
     (112, 48), (120, 48), (128, 48), (136, 48), (144, 48),
@@ -20,10 +22,14 @@ pwd_pos = (
 
 class App:
     def __init__(self):
-        self.pos = 0  # 上段の入力した文字が表示されている所のカーソル位置
-        self.cur = [0, 0]  # 下段の文字を選択する所のカーソル位置 [x, y]
+        # 上段の入力した文字が表示されている所のカーソル位置
+        self.pos = 0
+        # 下段の文字を選択する所のカーソル位置 [x, y]
+        self.cur = [0, 0]
+        # 入力した文字番号 -1は＊
         self.pwd = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -
                     1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+        # カーソルを点滅させるためのフラグ
         self.blink_flg = True
 
         pyxel.init(256, 240, caption='Dragon Quest ～ふっかつのじゅもん～')
@@ -56,13 +62,30 @@ class App:
             if self.cur[1] == 6 and (self.cur[0] == 5 or self.cur[0] == 7 or self.cur[0] == 9):
                 self.cur[0] -= 1
 
+        # スペースキーで文字決定
         if pyxel.btnp(pyxel.KEY_SPACE):
+            # ピッピ音
             pyxel.play(0, 0)
+
+            # 文字位置計算
             cur_pos = self.cur[0] + self.cur[1] * 10
+
+            # 文字の場合
             if cur_pos <= 63:
                 self.pwd[self.pos] = cur_pos
                 if self.pos < 19:
                     self.pos += 1
+            # もどる
+            elif cur_pos == 64:
+                if self.pos > 0:
+                    self.pos -= 1
+            # すすむ
+            elif cur_pos == 66:
+                if self.pos < 19:
+                    self.pos += 1
+            # おわり
+            elif cur_pos == 68:
+                print('おわり')
 
         # カーソルを点滅させる
         if pyxel.frame_count % 4 == 0:
@@ -76,6 +99,7 @@ class App:
         if self.blink_flg:
             pyxel.blt(56 + self.cur[0] * 16, 104 + self.cur[1] * 16, 0, 96, 24, 8, 8)
 
+        # 上段入力文字下のカーソル
         pyxel.blt(pwd_pos[self.pos][0], pwd_pos[self.pos][1] + 8, 0, 88, 16, 8, 8)
 
         # 入力したふっかつのじゅもん
